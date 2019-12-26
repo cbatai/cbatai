3e75fc425538090309c40cb91c9365a93c6bec6b
billy harvey <billy.harvey@gmail.com>
2019-12-26 15:29:53 -0500

    .

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('/service.js').then(function(registration) {
       // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
 	    if (navigator.serviceWorker.controller == null) {
+      		console.log('ServiceWorker registration successful with scope: ', registration.scope);
 		window.location = ".";
+	    } else {
+		    main();
 	    }
     }, function(err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     })
   })
+}
+
+function main() {
+	console.log('hello world');
 }
